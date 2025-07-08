a009f943b5be557b5cdd3919bf4c6a7b72706dfb
joannalai <llai_joanna@sina.com>
2025-07-08 11:44:21 +0800

    addDiv

--- src/git.jsx
+++ src/git.jsx
@@ -1,8 +1,11 @@
+import './git.css'
+
 export default function App(){
     return(
         <div className="app">
             this is app
             <h1>上传远程</h1>
+            <div className="box"></div>
         </div>
     )
 }
